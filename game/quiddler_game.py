a9441292a78a5144ef6fceb0ecf05c2bc169d56e
Jeremy Blythe <jblythe@evertz.com>
2020-11-28 00:36:59 -0500

    Fixes. Now runs for the examples at least.

--- game/quiddler_game.py
+++ game/quiddler_game.py
@@ -1,4 +1,3 @@
-import math
 import pygtrie as trie
 
 card_points = {'a':2,'b':8,'c':8,'d':5,'e':2,'f':6,'g':6,'h':7,'i':2,'j':13,'k':8,
@@ -9,35 +8,49 @@ double_cards = set(list(card_points.keys())[-5:])
 single_cards = set(list(card_points.keys())[:-5])
 double_card_first = {d[0]:d[1] for d in double_cards}
 
-# Build data structure for all possible words
+def make_perm(word, card_list, pos, collector):
+    '''
+    Given a word, break it into all the ways to make that word using the cards and get the
+    total score for each permutation.
+
+    For example the word: "inquiring" can be constructed from the cards in 8 ways: 
+        {'in/qu/i/r/in/g': 36,
+        'in/qu/i/r/i/n/g': 36,
+        'in/q/u/i/r/in/g': 46,
+        'in/q/u/i/r/i/n/g': 46,
+        'i/n/qu/i/r/in/g': 36,
+        'i/n/qu/i/r/i/n/g': 36,
+        'i/n/q/u/i/r/in/g': 46,
+        'i/n/q/u/i/r/i/n/g': 46}
+
+    This is a recursive function.
+        card_list holds the cards as they're identified for this word
+        pos is the character position through the word
+        collector is a dict where the results are stored
+    '''
+    while pos < len(word):
+        if pos < len(word)-1:
+            # check for a double letter card
+            dc = word[pos]+word[pos+1]
+            if dc in double_cards:
+                # found a double letter card so fork here
+                make_perm(word, card_list.copy()+[dc], pos+2, collector)
+        card_list+=[word[pos]]
+        pos+=1
+    # words have to be made from 2 to 10 cards so only collect these
+    if len(card_list) >= 2 and len(card_list) <= 10:
+        collector['/'.join(card_list)] = sum([card_points[c] for c in card_list])
+
+# Construct the Prefix Tree for all possible word/card permutations
 with open("sowpods.txt", "r") as text_file:
     lines = text_file.readlines()
-words = [w.strip() for w in lines[6:]]
-print(len(lines))
-
-def make_perm(s, clist, pos, collector):
-    # move forward in the list through s until hitting a branching point when we call ourselves with copy of clist
-    while pos < len(s):
-        if pos < len(s)-1:
-            # check for a double
-            dc = s[pos]+s[pos+1]
-            if dc in double_cards:
-                make_perm(s, clist.copy()+[dc], pos+2, collector)
-        clist+=[s[pos]]
-        pos+=1
-    # words have to be made from 2 to 10 cards
-    if len(clist) >= 2 and len(clist) <= 10:
-        collector['/'.join(clist)] = sum([card_points[c] for c in clist])
-
-perms = {}
-for s in words:
-    make_perm(s,[],0,perms)
-
-t = trie.StringTrie(separator='/')
-t.update(perms)
+perms = trie.StringTrie(separator='/')
+for w in lines[6:]:
+    make_perm(w.strip(),[],0,perms)
+print(f'Using {len(perms)} possible word permutations\n')
 
 def find_word(wt, p, hand_left, collector):
-    for idx,c in enumerate(hand_left):
+    for idx, _ in enumerate(hand_left):
         h=hand_left.copy()
         tp = p+[h.pop(idx)]
         s = '/'.join(tp)
@@ -46,19 +59,6 @@ def find_word(wt, p, hand_left, collector):
             if score:
                 collector[s] = (score, len(tp), tp)
             find_word(wt,tp,h,collector)
-
-# Find all possible words for a hand
-hand = ['x','k','r','s','u','m','th','e','s','a']
-possible_words = {}
-find_word(t,[],hand,possible_words)
-
-len(possible_words), sorted(possible_words.values(),reverse=True)
-
-# Find best scoring combination
-# first find all the valid combinations from the possible list
-# then for each combo subtract points for unused cards (remember this subtraction amount)
-pwt = trie.StringTrie(separator='/')
-pwt.update({x[0]: x[1][0] for x in possible_words.items()})
 
 def scored_play(combo, hand_left):
     ts, tc = 0, 0
@@ -82,44 +82,43 @@ def find_word_combos(wt, combo, hand_left, collector):
         find_word_combos(wt,cc,j,collector)
     collector.append(scored_play(combo,hand_left))
 
-
-combos = []
-for w in possible_words.items():
-    j = hand.copy()
-    for c in w[1][2]:
-        j.remove(c)
-    find_word_combos(pwt,[w],j,combos)
-
-sorted(combos, reverse=True)[0]
-
 def get_play(hand):
     possible_words = {}
-    find_word(t,[],hand,possible_words)
+    find_word(perms,[],hand,possible_words)
+    pwt = trie.StringTrie(separator='/')
+    pwt.update({x[0]: x[1][0] for x in possible_words.items()})
     combos = []
     for w in possible_words.items():
         j = hand.copy()
         for c in w[1][2]:
             j.remove(c)
         find_word_combos(pwt,[w],j,combos)
-    return sorted(combos, reverse=True)[0]
+    return sorted(combos, reverse=True)[0] if len(combos) > 0 else None
 
-hand = ['e','e','a','y','qu','cl','n','u','w','s']
+def get_best_play(hand, pick_up):
+    print(f'Hand: {"/".join(hand)}')
+    print(f'Deck: {pick_up}')
+    no_pick_up = get_play(hand)
+    options = []
+    # Tuple is: (Hand, Card picked up, Card dropped)
+    # First option is to not pick up hence the card picked up and dropped are the same
+    if no_pick_up: 
+        options.append((no_pick_up, pick_up, pick_up))
+    for idx, _ in enumerate(hand):
+        hc = hand.copy()
+        hc[idx] = pick_up
+        p = get_play(hc)
+        if p:
+            options.append((p, pick_up, hand[idx]))
+    if options:
+        play = sorted(options, reverse=True)[0]
+        if play[0]:
+            print(f'Score: {play[0][0]}')
+            print(f'Complete: {play[0][1]}')
+            print(f'Words: {[s[0] for s in play[0][2]]}')
+        print(f'Pick up: {play[1]}')
+        print(f'Drop: {play[2]}\n')
 
-no_pick_up = get_play(hand)
-options = [(no_pick_up, pick_up, pick_up)]
-pick_up = 'h'
-for idx,h in enumerate(hand):
-    hc = hand.copy()
-    hc[idx] = pick_up
-    options.append((get_play(hc), pick_up, hand[idx]))
-play = sorted(options, reverse=True)[0]
 
-play
-
-print(f'Score: {play[0][0]}')
-print(f'Complete: {play[0][1]}')
-print(f'Words: {[s[0] for s in play[0][2]]}')
-print(f'Pick up: {play[1]}')
-print(f'Drop: {play[2]}')
-
-
+get_best_play(['qu','e','b','n','z'],'s')
+get_best_play(['i','c','e','v','i','s','i','o','n'],'i')
